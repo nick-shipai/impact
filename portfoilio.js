920a0933c43d0625e849a49226bb1ad86d538f66
nick-shipai <mercydav911@gmail.com>
2026-02-13 19:29:48 +0100

    Add files via upload

--- portfoilio.js
+++ portfoilio.js
@@ -20,3 +20,26 @@ window.addEventListener('scroll', revealOnScroll);
 
 // Trigger animation on load for elements already visible
 window.addEventListener('load', revealOnScroll);
+
+// Get modal elements
+const modal = document.getElementById("contactModal");
+const btn = document.querySelector(".outline"); // "Get In Touch" button
+const span = document.querySelector(".close");
+
+// Open modal on button click
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Close modal on X click
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Close modal if clicked outside the content
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
